d4385c4107444e903e9cd7d31a7b75f8f5695e58
Peter Yost <peter.yost@intive.com>
2024-06-23 16:22:51 +0200

    add Router and Components

---
+++ src/Display.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class Display extends React.Component {
+    render() {
+        return <h2>Hi, I am The Display!</h2>;
+    }
+}
---
+++ src/Judge.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class Judge extends React.Component {
+    render() {
+        return <h2>Hi, I am a Car!</h2>;
+    }
+}
